78a3a973c549b08da41f70c4d71e44321b20145c
Arin Bakht <hello@arinbakht.ca>
2019-04-16 11:58:47 -0400

    Updated for May 2019
    Changed all references to old dates and times to new dates and times. Removed all old course descriptions, uploaded template course description in their place (plus second template course to test calendar functionality)

--- docs/_includes/fullcalendar.js
+++ docs/_includes/fullcalendar.js
@@ -23,7 +23,7 @@ $(document).ready(function(){
 			center: 'title',
 			right: 'agendaWeek,agendaDay'
 		},
-		defaultDate: '2018-11-19',
+		defaultDate: '2019-05-27',
 		minTime: '08:00:00', // these two lines fix the viewable area to business
 		maxTime: '17:30:00', // hours and make it not scrollable
 		contentHeight: 500,
